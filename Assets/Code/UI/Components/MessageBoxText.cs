3bbdc9441ec0e20d2647aa0dacc15a30b3501f14
rsmith52 <rsmith52@wisc.edu>
2024-10-17 20:07:28 +1300

    working on understanding data bindings in new UI Toolkit

---
+++ Assets/Code/UI/Components/MessageBoxText.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UI.Components
+{
+    [UxmlElement]
+    public partial class MessageBoxText : Label
+    {
+        #region Fields
+
+        public static BindingId keyProperty = nameof(message_text);
+
+        [UxmlAttribute]
+        public string message_text;
+
+        #endregion
+
+        public MessageBoxText()
+        {
+
+        }
+
+    }
+}
---
+++ Assets/Code/UI/MessageBox.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace UI
+{
+    public class MessageBox : MonoBehaviour
+    {
+        #region Fields
+
+        [Title("Message Text", bold: false)]
+        [HideLabel]
+        [MultiLineProperty(3)]
+        public string message_text;
+
+        #endregion
+    }
+}
